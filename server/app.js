ecb68ab8d550a1d76cd58ed759bf9bb3778fbb3d
ali <alichocr2@gmail.com>
2025-08-22 15:17:04 +0100

    add boilerplate for app.js and index.js and rename mvc js files

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+
+const locationRouter = require('./routes/location')
+const logger = require('./middleware/logger')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(logger)
+
+app.use('/location', locationRouter)
+
+module.exports = app
