65decb4355a798474678f40e4373c5f69dc210cb
angentidea <angentidea@gmail.com>
2025-08-26 09:36:50 +0100

    creates get routes

--- server/routes/locations.js
+++ server/routes/locations.js
@@ -2,7 +2,7 @@ const { Router } = require('express')
 
 const locationRouter = Router()
 
-module.exports = locationRouter
-
 locationRouter.get("/", locationRouter.getCountry);
-locationRouter.get("/:country", locationRouter.revealCountry)
+locationRouter.get("/:country", locationRouter.revealCountry);
+
+module.exports = locationRouter;
